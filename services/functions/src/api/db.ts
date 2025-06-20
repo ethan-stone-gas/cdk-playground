252afb71b864b9ad9ee06c18750f38bd8f6b3b23
ethan-stone-gas <estone@greenactionstudio.com>
2025-06-20 13:29:23 -0400

    make services

--- services/functions/src/api/db.ts
+++ services/functions/src/api/db.ts
@@ -1,79 +1,74 @@
 import { MongoClient } from "mongodb";
 import { type Comment, type Ticket } from "./schemas";
 
+type MongoTicket = Omit<Ticket, "id"> & { _id: string };
+type MongoComment = Omit<Comment, "id"> & { _id: string };
+
+const toMongo = {
+  ticket: (ticket: Ticket) => {
+    return {
+      ...ticket,
+      _id: ticket.id,
+    };
+  },
+  comment: (comment: Comment) => {
+    return {
+      ...comment,
+      _id: comment.id,
+    };
+  },
+};
+
+const fromMongo = {
+  ticket: (ticket: MongoTicket) => {
+    return {
+      ...ticket,
+      id: ticket._id,
+    };
+  },
+  comment: (comment: MongoComment) => {
+    return {
+      ...comment,
+      id: comment._id,
+    };
+  },
+};
+
 const client = new MongoClient(process.env.MONGO_URL!);
 
 const db = client.db("ticketing");
 
-const ticketCollection = db.collection("tickets");
-const commentCollection = db.collection("comments");
+const ticketCollection = db.collection<MongoTicket>("tickets");
+const commentCollection = db.collection<MongoComment>("comments");
 
-// Helper functions to convert between MongoDB _id and application id
-const toMongoDoc = <T extends { id: string }>(
-  doc: T
-): Omit<T, "id"> & { _id: string } => {
-  const { id, ...rest } = doc;
-  return { ...rest, _id: id };
-};
-
-const fromMongoDoc = (doc: any): any => {
-  if (!doc) return null;
-  const { _id, ...rest } = doc;
-  return { ...rest, id: _id };
-};
-
-export const syncTicket = async (ticket: Ticket) => {
-  const mongoDoc = toMongoDoc(ticket);
-  await ticketCollection.updateOne(
-    { _id: mongoDoc._id } as any,
-    { $set: mongoDoc },
-    { upsert: true }
-  );
-};
-
-export const syncComment = async (comment: Comment) => {
-  const mongoDoc = toMongoDoc(comment);
-  await commentCollection.updateOne(
-    { _id: mongoDoc._id } as any,
-    { $set: mongoDoc },
-    { upsert: true }
-  );
-};
-
-// Helper functions to get documents by id
-export const getTicketById = async (id: string): Promise<Ticket | null> => {
-  const mongoDoc = await ticketCollection.findOne({ _id: id } as any);
-  return fromMongoDoc(mongoDoc);
-};
-
-export const getCommentById = async (id: string): Promise<Comment | null> => {
-  const mongoDoc = await commentCollection.findOne({ _id: id } as any);
-  return fromMongoDoc(mongoDoc);
-};
-
-// Helper functions to get multiple documents
-export const getTickets = async (
-  filter: Partial<Ticket> = {}
-): Promise<Ticket[]> => {
-  const { id, ...restFilter } = filter;
-  const mongoFilter: any = { ...restFilter };
-  if (id) {
-    mongoFilter._id = id;
+export class TicketService {
+  public async upsertTicket(ticket: Ticket) {
+    const mongoTicket = toMongo.ticket(ticket);
+    await ticketCollection.updateOne(
+      { _id: mongoTicket._id },
+      { $set: mongoTicket },
+      { upsert: true }
+    );
   }
 
-  const mongoDocs = await ticketCollection.find(mongoFilter).toArray();
-  return mongoDocs.map(fromMongoDoc);
-};
+  public async getTicket(id: string) {
+    const mongoTicket = await ticketCollection.findOne({ _id: id });
+    return mongoTicket ? fromMongo.ticket(mongoTicket) : null;
+  }
+}
 
-export const getComments = async (
-  filter: Partial<Comment> = {}
-): Promise<Comment[]> => {
-  const { id, ...restFilter } = filter;
-  const mongoFilter: any = { ...restFilter };
-  if (id) {
-    mongoFilter._id = id;
+export class CommentService {
+  public async upsertComment(comment: Comment) {
+    const mongoComment = toMongo.comment(comment);
+    await commentCollection.updateOne(
+      { _id: mongoComment._id },
+      { $set: mongoComment },
+      { upsert: true }
+    );
   }
 
-  const mongoDocs = await commentCollection.find(mongoFilter).toArray();
-  return mongoDocs.map(fromMongoDoc);
-};
+  public async getComment(id: string) {
+    const mongoComment = await commentCollection.findOne({ _id: id });
+    return mongoComment ? fromMongo.comment(mongoComment) : null;
+  }
+}
